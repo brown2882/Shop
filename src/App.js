c890538dc7d73fbeeda6e966dbaef646fcbe3bd0
brown2882 <markbrown282828@gmail.com>
2023-08-03 09:32:09 +0400

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import './assets/style/shop.scss'
 import {Home} from "./pages/home";
 import {useEffect, useState} from "react";
 import RolexOne from './assets/images/rolex1.jpg'
@@ -11,30 +12,45 @@ function App() {
         {watch:RolexTwo, id:2},
         {watch:RolexTree, id:3}
     ])
-
-
     const [loading,setloading] = useState(true)
-
-
-
     useEffect(() => {
         setTimeout (() => {
             setloading(false)
-        }, 3000)
+        }, 1000)
     },[])
+    const[number, setnumber] = useState(0)
+    const plusNumber = () => {
+        setnumber(number + 1)
+    }
+    const minusNumber = () => {
+        setnumber(number - 1)
+    }
+
+
   return (
     <div className="App">
         {loading === true
         ?
             <p>Loading...</p>
         :
-        <>
-            {photo?.map((i,j) => (
+        <div style={{display:"flex"}}>
+            {photo.map((i, j) => (
                 <div key={j}>
-                    <div>{i?.watch}</div>
+                    <div className='mainDiv'>
+                        <img style={{height:400, margin:25}} src={i.watch} alt={`Rolex ${i.id}`} />
+                        <div style={{paddingLeft:40}}>
+                        <h1>Whatch for Man</h1>
+                        <h2>1000$</h2>
+                        </div>
+                        <div style={{display:"flex",paddingLeft:60}}>
+                             <button id={j + 1} onClick={plusNumber}>+</button>
+                             <p>{number}</p>
+                             <button onClick={minusNumber}>-</button>
+                         </div>
+                     </div>
                 </div>
             ))}
-        </>
+        </div>
         }
     </div>
   );
